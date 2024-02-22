14bd73951bb743f04b041d1e02ebffe917446aae
YoheiHayamizu <hayamizu0111@gmail.com>
2024-02-22 12:10:48 -0500

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pddlgym',
-      version='0.0.7',
+      version='0.0.8',
       install_requires=['matplotlib', 'pillow', 'gymnasium', 'imageio', 'scikit-image'],
       packages=find_packages(),
       include_package_data=True,
